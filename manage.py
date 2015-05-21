94aa3fd5e4c840eb4d09ec11e33fa1d9954dc1da
Adam Shimali <adam.shimali@gmail.com>
2015-05-21 14:50:58 +0100

    fix manage

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from flask.ext.script import Server, Manager
 from app import app
 
 manager = Manager(app)
-manager.add_option("-c", "--config", dest="config_filename", required=True)
 port = int(os.environ.get('PORT', 8000))
 manager.add_command("runserver", Server(host='0.0.0.0', port=port))
 
